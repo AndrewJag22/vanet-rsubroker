22f4da327ee8adc6e9fa147c8c5c02d8d7d7d6a3
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-18 05:45:14 +0100

    created crontab to run scripts

---
+++ broker_crontab.sh
@@ -0,0 +1,17 @@
+# Generates private key used for blockchain
+python3 private_keygenerator.py
+
+# Creates the broker's key and csr for mqtt connection
+./broker_csr_key_gen.sh
+
+# Sends the created crs to the CA for certification
+./mqttbrokerca.sh
+
+# Creates cron jobs to perform mqtt subscriber setup and run the blockchain whenever the server is put on
+crontab -l > current_cron
+cat >> current_cron << EOF
+@reboot python3 mqtt_subscriber_setup.py
+@reboot python3 rsublockchain.py
+EOF
+crontab < current_cron
+rm -f current_cron
