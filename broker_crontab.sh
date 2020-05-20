e62b9bb7b1265f36d7cb1b048d91817ff096e588
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-20 06:09:39 +0100

    made changes for better configuration

--- broker_crontab.sh
+++ broker_crontab.sh
@@ -1,5 +1,5 @@
 # Generates private key used for blockchain
-python3 private_keygenerator.py
+python3 private_key_generator.py
 
 # Creates the broker's key and csr for mqtt connection
 ./broker_csr_key_gen.sh
@@ -10,8 +10,20 @@ python3 private_keygenerator.py
 # Creates cron jobs to perform mqtt subscriber setup and run the blockchain whenever the server is put on
 crontab -l > current_cron
 cat >> current_cron << EOF
-@reboot python3 mqtt_subscriber_setup.py
-@reboot python3 rsublockchain.py
+@reboot /etc/mosquitto/./broker_server.exp
+@reboot /etc/blockchain/./sub_script.exp
+@reboot python3 /etc/blockchain/rsu_blockchain.py
 EOF
 crontab < current_cron
 rm -f current_cron
+
+# Creates copies of mqtt broker and subscriber and, blockchain executable files
+cp broker_server.exp /etc/mosquitto/./broker_server.exp
+cp sub_script.exp /etc/blockchain/sub_script.exp
+cp mqtt_subscriber_setup.py /etc/blockchain/mqtt_subscriber_setup.py
+cp rsu_blockchain.py /etc/blockchain/rsu_blockchain.py
+
+# Starts up the mqtt broker and subscriber and, blockchain
+/etc/mosquitto/./broker_server.exp 
+/etc/blockchain/./sub_script.exp
+python3 /etc/blockchain/rsu_blockchain.py
