bc6f21bbd4c3ea2fe3438fd7566b7a74a3d42669
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-30 18:49:59 +0100

    made changes to include services

--- broker_crontab.sh
+++ broker_crontab.sh
@@ -1,8 +1,15 @@
 BLOCKCHAINDIR=/etc/blockchain
 
-if test -f "$BLOCKCHAINDIR"; then
-    sudo mkdir "$BLOCKCHAINDIR"   
+if test -e "$BLOCKCHAINDIR"; then
+    echo "/etc/blockchain directory already exists"
+else
+    mkdir "$BLOCKCHAINDIR"
+    echo "Created /etc/blockchain directory"
 fi
+
+# Adds executable attribute to scripts
+chmod +x broker_csr_key_gen.sh
+chmod +x mqttbrokerca.sh
 
 # Generates private key used for blockchain
 python3 private_key_generator.py
@@ -13,23 +20,35 @@ python3 private_key_generator.py
 # Sends the created crs to the CA for certification
 ./mqttbrokerca.sh
 
-# Creates cron jobs to perform mqtt subscriber setup and run the blockchain whenever the server is put on
-crontab -l > current_cron
-cat >> current_cron << EOF
-@reboot /etc/mosquitto/./broker_server.exp
-@reboot /etc/blockchain/./sub_script.exp
-@reboot python3 /etc/blockchain/rsu_blockchain.py
-EOF
-crontab < current_cron
-rm -f current_cron
-
 # Creates copies of mqtt broker and subscriber and, blockchain executable files
 cp broker_server.exp /etc/mosquitto/./broker_server.exp
 cp sub_script.exp /etc/blockchain/sub_script.exp
 cp mqtt_subscriber_setup.py /etc/blockchain/mqtt_subscriber_setup.py
 cp rsu_blockchain.py /etc/blockchain/rsu_blockchain.py
 
-# Starts up the mqtt broker and subscriber and, blockchain
-/etc/mosquitto/./broker_server.exp 
-/etc/blockchain/./sub_script.exp
-python3 /etc/blockchain/rsu_blockchain.py
+# Copies services to /lib/systemd/system folder
+cp rsu_blockchain.service /lib/systemd/system/rsu_blockchain.service
+cp sub_script.service /lib/systemd/system/sub_script.service
+cp broker.service /lib/systemd/system/broker.service
+
+# Creates cron jobs to perform mqtt broker and subscriber setup and run the blockchain whenever the server is put on
+crontab -l > current_cron
+cat >> current_cron << EOF
+@reboot systemctl start sub_script.service
+@reboot systemctl start rsu_blockchain.service
+@reboot systemctl start broker.service
+EOF
+crontab < current_cron
+rm -f current_cron
+
+# Starts up the mqtt broker and subscriber and, blockchain services
+systemctl daemon-reload
+
+systemctl enable sub_script.service
+systemctl start sub_script.service
+
+systemctl enable rsu_blockchain.service
+systemctl start rsu_blockchain.service
+
+systemctl enable broker.service
+systemctl start broker.service
