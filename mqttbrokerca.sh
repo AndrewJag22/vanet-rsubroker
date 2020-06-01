70ef39b24796eb776aed9b5a5527960f7b3f45c9
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-06-01 01:03:58 +0100

    minor tweak to allow for input of CAserverIP

--- mqttbrokerca.sh
+++ mqttbrokerca.sh
@@ -1,8 +1,9 @@
 export SSHPASS=vanetclients # Password to connect to sftp client profile of certification authority
+CAserverIP=
 cd /etc/certs
 echo "Sending CSR to CA server"
 # Connects to the sftp client for certification at username@ca_server_name to submit csr
-sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
+sshpass -e sftp -oBatchMode=no -b - vanetclients@$CAserverIP << !
    cd incoming_requests
    put broker.csr
    bye
@@ -10,7 +11,7 @@ sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
 echo "Receiving CA and client certificates"
 sleep 1
 # Connects to the sftp client for certification at username@ca_server_name to collect CA certificate and user certificate
-sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
+sshpass -e sftp -oBatchMode=no -b - vanetclients@$CAserverIP << !
    get ca.crt
    cd outgoing_certificates
    get broker.crt
